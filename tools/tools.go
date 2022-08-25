8ef305e1e7362396beb6cffc1d77c9f058321f78
Xavier Godard <xavier.godard@xdesign.com>
2022-08-25 22:02:04 +0100

    Add docs, goreleaser, and GH actions

---
+++ tools/tools.go
@@ -0,0 +1,10 @@
+//go:build tools
+// +build tools
+
+package tools
+
+import (
+	// document generation
+	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+)
+gpg --armor --export-secret-keys "Xavier <xavgodard@gmail.com>"
